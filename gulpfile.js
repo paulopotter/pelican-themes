095b3282e49a288cb733e7d9caff5f5ed3322006
Paulo Oliveira <paulo.oliveira@corp.globo.com>
2016-03-28 13:11:12 -0300

    Corrige gulpfile para concatenar css

--- gulpfile.js
+++ gulpfile.js
@@ -1,12 +1,14 @@
 var gulp = require('gulp'),
     sass = require('gulp-sass'),
     rename = require('gulp-rename'),
-    minifyCSS = require('gulp-minify-css');
+    minifyCSS = require('gulp-minify-css'),
+    concat = require('gulp-concat');
 
 gulp.task('styles', function(){
 return gulp.src('minimal-dev/static/scss/*.scss')
     .pipe(sass()) 
     .pipe(gulp.dest('minimal-dev/static/css/'))
+    .pipe(concat("style.css"))
     .pipe(minifyCSS())
     .pipe(rename('style.min.css'))
     .pipe(gulp.dest('minimal-dev/static/css/'))
